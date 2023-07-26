bfeed6d299779d3276761e4007b43178eedf4378
Nicole <lancasternj@gmail.com>
2023-07-26 19:29:55 +0100

    initialisiing repo and connecting MongoDB db

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("./package.json");
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
