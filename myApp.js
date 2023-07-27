ccafbca81ed29722755864f7607b2303fff3b11f
Nicole <lancasternj@gmail.com>
2023-07-27 15:52:41 +0100

    creating a new person schema and saving person to the database

--- myApp.js
+++ myApp.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+const Schema = mongoose.Schema;
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favouriteFoods: [String],
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  done(null, Person);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
